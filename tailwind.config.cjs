2e35a4bb987db413271371ebe3d3bd466266577d
Slavik6661 <slavik0.75@mail.ru>
2026-03-25 13:22:15 +0300

    add fix sort img add loader img

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "neutral-tertiary": "#6B7280",
+        brand: "#3B82F6",
+      },
+    },
   },
   plugins: [],
 };
